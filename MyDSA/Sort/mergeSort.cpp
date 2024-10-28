218a01fec588f978b22e323b7cb6426bdb32a6cf
vasundhra gupta <vasundhragupta962@gmail.com>
2024-10-28 19:26:00 +0530

    refinement

--- MyDSA/Sort/mergeSort.cpp
+++ MyDSA/Sort/mergeSort.cpp
@@ -7,35 +7,44 @@ void printArray(int arr[], int n)
     {
         cout << arr[i] << "\t";
     }
-    cout<<endl;
+    cout << endl;
 }
 
 void mergeSort(int arr[], int low, int mid, int high)
 {
     int i = low;
-    int j = mid;
+    int j = mid + 1;
     int k = low;
-    int b[high];
-    while (i < mid && j < high)
+    int b[high+1];
+    while (i <= mid && j <= high)
     {
         if (arr[i] < arr[j])
         {
-            b[k] = arr[i];
-            i++;
+            b[k] = arr[i++];
         }
         else
         {
-            b[k] = arr[j];
-            j++;
+            b[k] = arr[j++];
         }
         k++;
     }
+
+    while (i <= mid)
+    {
+        b[k++] = arr[i++];
+    }
+
+    while (j <= high)
+    {
+        b[k++] = arr[j++];
+    }
+
     printArray(b, high);
 }
 int main()
 {
     int arr[] = {1, 3, 5, 7, 9, 0, 2, 4, 6, 8};
     printArray(arr, 10);
-    mergeSort(arr, 0, 5, 10);
+    mergeSort(arr, 0, 5, 9);
     return 0;
 }
