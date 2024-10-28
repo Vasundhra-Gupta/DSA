e50284ab20d8b68a68848e275dd11ceaed4d43bc
vasundhra gupta <vasundhragupta962@gmail.com>
2024-10-28 19:25:11 +0530

    mergeSort, symFibonacci, binarySearch

--- Recursion/binarySearch.cpp
+++ Recursion/binarySearch.cpp
@@ -2,42 +2,39 @@
 #include <math.h>
 using namespace std;
 
-void insertionSort(int arr[], int n){
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout<<endl;
+}
+
+void insertionSort(int a[], int n){
     for(int i=1; i<n; i++){
-        int key = arr[i];
-        int j= i-1;
-        while(j>0 && arr[j]>key){
-            arr[j+1] = arr[j];
+        int key= a[i];
+        int j=i-1;
+        while(a[j]>key && j>=0){
+            a[j+1]= a[j];
             j--;
         }
-        arr[j+1] = key;
+        a[j+1] = key;
     }
 }
 
-
 int binarySearch(int arr[], int low, int high, int search)
 {
-    int mid = ceil((low+high)/2);
-    bool isFound = false;
+    int mid = (low+high)/2;
     if(search == arr[mid]){
-        isFound = true;
         return mid;
     }
-    if(low == high){
-        isFound = true;
-        return low;
-    }
+
     if(search < arr[mid]){
         binarySearch(arr, 0, mid-1, search);
     }else{
         binarySearch(arr, mid+1, high-1, search);
-    }
-
-    if(!isFound){
-        cout<<"Number not found\n";
-        return -1;
-    }
-    
+    }    
 }
 
 int main()
@@ -45,10 +42,14 @@ int main()
     int arr[] = {2, 5, 7, 9, 3, 6, 0, 1, 7};
     int n = sizeof(arr)/sizeof(arr[0]);
     insertionSort(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<"\t";
+    }
+    cout<<endl;
     int search;
     cout<<"Enter the element to be searched:\n";
     cin>>search;
     int pos= binarySearch(arr, 0, n, search);
-    cout<<"The element you searched is at position "<< pos<<endl;
+    cout<<"The element you searched is at position "<< pos+1<<endl;
     return 0;
 }
---
+++ Recursion/mergeSort.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int merge(int arr[], int low, int mid, int high)
+{
+    int i = low;
+    int j = mid+1;
+    int k = 0;
+
+    int* temp = new int[high - low + 1];
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+
+    while (j <= high)
+    {
+        temp[k] = arr[j++];
+        k++;
+    }
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i-low];
+        cout<<arr[i]<<"\t";
+    }
+    cout<<endl;
+}
+
+int mergeSort(int arr[], int low, int high)
+{
+    // if (low == high)
+    // {
+    //     return arr[low];
+    // }
+    if (low < high)
+    {
+        int mid = (high + low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 3, 9, 7, 8, 4, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    mergeSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<"\t";
+    }
+
+    cout<<endl;
+    return 0;
+}
